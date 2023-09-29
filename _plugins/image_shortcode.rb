7538c1c4c9e5cc0929eee729cb1d4c1eb3252baf
Stanislav Pyshevskyi <stas@metawork.studio>
2023-09-29 15:51:31 +0200

    #50: Modify the `flexible_image` shortcode

--- _plugins/image_shortcode.rb
+++ _plugins/image_shortcode.rb
@@ -53,6 +53,8 @@
 
 # Shortcode - {% flexible_image link="/2/IMG_8148.png" alt="Alt text" class="img-class" %}
 
+require 'pp'
+
 module Jekyll
   class ImageTag < Liquid::Tag
     def initialize(tag_name, markup, tokens)
@@ -63,30 +65,44 @@ module Jekyll
     def parse_params(markup)
       params = {}
 #       markup.scan(/(\w+)\s*=\s*["']([^"']+)["']/) do |key, value|
- markup.scan(/(\w+)\s*=\s*["']([^"']*)["']/) do |key, value|
+  markup.scan(/(\w+)\s*=\s*["']([^"']*)["']/) do |key, value|
         params[key] = value
       end
       params
     end
 
     def render(context)
+      file_path = /\/chapters\/(?<path>.*)\//.match(context['page']['url'])
+      file_path = file_path ? file_path['path'].gsub('/sections', '').sub(/^\//, '').sub(/\/$/, '').gsub('.', '-').strip : nil
+      file_path = file_path ? "/#{file_path}/" : ''
+
       baseurl = context['site']['baseurl']
       image_folder = "#{baseurl}/assets/images"
 
       filename = @params['link'] || ''
       alt_text = @params['alt'] || ''
       css_classes = @params['class'] || ''
+      PP.pp(context['page']['url'])
+      #PP.pp(file_path)
+
 
       if context['page']['url'].include?('tablet') && !filename.empty?
-       tablet_path = filename.sub(%r{\/(\d+)\/}, '/\1/tablet/')
-        image_tag = "<a href=\"#{image_folder}#{tablet_path}\" data-fancybox=\"gallery\" class=\"fancybox\">"
-        image_tag += "<img src=\"#{image_folder}#{tablet_path}\" alt=\"#{alt_text}\" class=\"#{css_classes}\" />"
+        # New tablet path
+        tablet_path = "#{image_folder}#{file_path}tablet/#{filename}"
+        PP.pp(tablet_path)
+
+        tablet_path = filename.sub(%r{\/(\d+)\/}, '/\1/tablet/')
+        image_tag = "<a href=\"#{tablet_path}\" data-fancybox=\"gallery\" class=\"fancybox\">"
+        image_tag += "<img src=\"#{tablet_path}\" alt=\"#{alt_text}\" class=\"#{css_classes}\" />"
         image_tag += "</a>"
       else
-        image_tag = "<a href=\"#{image_folder}#{filename}\" data-fancybox=\"gallery\" class=\"fancybox\">"
-        image_tag += "<img src=\"#{image_folder}#{filename}\" alt=\"#{alt_text}\" class=\"#{css_classes}\" />"
+        image_path = "#{image_folder}#{file_path}#{filename}"
+        image_tag = "<a href=\"#{image_path}\" data-fancybox=\"gallery\" class=\"fancybox\">"
+        image_tag += "<img src=\"#{image_path}\" alt=\"#{alt_text}\" class=\"#{css_classes}\" />"
         image_tag += "</a>"
       end
+
+      puts "\n"
 
       image_tag
     end
